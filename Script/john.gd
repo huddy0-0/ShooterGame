c16ba05d0cac0d8188cfca7f1a20fe1f0a85ff7e
Hudson Shelfer <hudson.shelfer@gmail.com>
2025-04-26 23:27:59 -0400

    initial commit

---
+++ Script/john.gd
@@ -0,0 +1,88 @@
+extends CharacterBody3D
+
+var player = null
+var state_machine
+var propel_force = Vector3.ZERO
+
+
+const SPEED = 4.0
+const ATTACK_RANGE = 1.5
+const PROPEL_STRENGTH = 8.0
+
+@export var player_path := "../Player"
+
+@onready var nav_agent = $NavigationAgent3D
+@onready var anim_tree = $AnimationTree
+@onready var palmer_texture = preload("res://IMG_86141.jpg")
+@onready var john_texture = preload("res://IMG_86142.jpg")
+@onready var aiden_texture = preload("res://IMG_86143.jpg")
+
+
+
+func _ready():
+	player = get_node(player_path)
+	state_machine = anim_tree.get("parameters/playback")
+	
+	var sphere = $Armature/Skeleton3D/Head2Attachment3D/CSGSphere3D
+	_randomize_texture(sphere)
+
+func _process(delta):
+	if player == null:
+		return
+	
+	var movement_velocity = Vector3.ZERO
+	
+	match state_machine.get_current_node():
+		"Walk":
+			nav_agent.set_target_position(player.global_transform.origin)
+			var next_nav_point = nav_agent.get_next_path_position()
+			movement_velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
+			look_at(Vector3(global_position.x + movement_velocity.x, global_position.y, global_position.z + movement_velocity.z), Vector3.UP)
+		"DropKick":
+			var direction = (player.global_position - global_position).normalized()
+			movement_velocity = direction * (SPEED * 0.5)  
+			look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	movement_velocity += propel_force
+	
+	propel_force = propel_force.lerp(Vector3.ZERO, delta * 5.0)
+	
+	velocity = movement_velocity
+	move_and_slide()
+	
+	#Conditions
+	anim_tree.set("parameters/conditions/attack", _target_in_range())
+	anim_tree.set("parameters/conditions/run", !_target_in_range())
+	
+	anim_tree.get("parameters/playback")
+
+func _target_in_range():
+	if player == null:
+		return false
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
+	
+
+func _hit_finished():
+	if global_position.distance_to(player.global_position) < ATTACK_RANGE + 1.0:
+		var dir = global_position.direction_to(player.global_position)
+		player.hit(dir)
+
+func propel_forward():
+	if player == null:
+		return
+	var direction = (player.global_position - global_position).normalized()
+	propel_force = direction * PROPEL_STRENGTH
+
+func _randomize_texture(sphere):
+	if sphere.material_override == null:
+		sphere.material_override = StandardMaterial3D.new()
+		
+	var random_value = randi() % 3
+	var chosen_texture
+	match random_value:
+		0: chosen_texture = palmer_texture
+		1: chosen_texture = john_texture
+		3: chosen_texture = aiden_texture
+		_: chosen_texture = john_texture
+	
+	sphere.material_override.albedo_texture = chosen_texture
---
+++ Script/player.gd
@@ -0,0 +1,119 @@
+extends CharacterBody3D
+
+var noclip_enabled = false
+var speed
+var external_force = Vector3.ZERO
+
+
+const WALK_SPEED = 5.0    
+const SPRINT_SPEED = 8.0                                                                            
+const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.003
+const HIT_STAGGER = 4.0
+const PROPEL_STRENGTH = 8.0
+
+const BASE_FOV =  75.0                                                                              #Player FOV value
+const FOV_CHANGE = 1.5                                                                              #Value for changing FOV (ie while sprinting)
+
+signal player_hit                                                                          
+
+const BOB_FREQ = 2.0                                                                                #Head bob frequency
+const BOB_AMP = 0.08                                                                                #Head bob amplitude
+var t_bob = 0.0                                                                                     #Variable for passing how far along a sine wave the head is
+
+var gravity = 9.8
+
+@onready var playerHead = $PlayerHead                                                               #Save the PlayerHead node for access
+@onready var camera = $PlayerHead/Camera3D                                                          #Save the Camera node for access
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)                                                 #Hides/Locks mouse on start
+
+
+#Handle Head Rotation
+func _unhandled_input(event):                                                                       #Function called when any action is made by player
+	if event is InputEventMouseMotion:                                                              #If there is mouse motion
+		playerHead.rotate_y(-event.relative.x * SENSITIVITY)                                        #Rotate PlayerHead around the y axis relative to mouse movement along the x axis
+		camera.rotate_x(-event.relative.y * SENSITIVITY)                                            #Rotate Camera around x axis relative to mouse movement along the y acis
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(60))               #Sets the camera rotation along the x axis to a range of -40 to 60                         
+
+
+func _physics_process(delta):                                                                       #Built in function for gravity
+	#Unlock Mouse
+	if Input.is_action_just_pressed("unlock mouse"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	# Toggle Noclip
+	if Input.is_action_just_pressed("no_clip"):
+		noclip_enabled = !noclip_enabled
+	
+	#Handle Gravity
+	if not is_on_floor() and not noclip_enabled:                                                    #If player is not on the floor
+		velocity.y -= gravity * delta                                                               #Add velocity down based on gravity
+	
+	#Handle Jump
+	if Input.is_action_just_pressed("jump") and is_on_floor():                                      #If player presses jump action and is on the floor
+		velocity.y = JUMP_VELOCITY                                                                  #Set player velocity along the y axis to the jump velocity
+	
+	#Handle Sprint
+	if Input.is_action_pressed("sprint"):                                                           #If player is pressing sprint key
+		speed = SPRINT_SPEED                                                                        #Set speed to SPRINT_SPEED
+	else:                                                                                           #Otherwise, set speed to WALK_SPEED
+		speed = WALK_SPEED
+	
+	#Handle Movement
+	var input_velocity = Vector3.ZERO
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")                        #Built in player movement with custom actions
+	var direction = (playerHead.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if noclip_enabled:
+		if Input.is_action_pressed("up"):
+			velocity.y = speed
+		elif Input.is_action_pressed("down"):
+			velocity.y = -speed
+		else:
+			velocity.y = 0
+			
+	else:
+		if is_on_floor():                                                                           #If player is on the floor
+			if direction:                                                                           #Give player normal movement
+				input_velocity.x = lerp(velocity.x, direction.x * speed, delta * 10)
+				input_velocity.z = lerp(velocity.z, direction.z * speed, delta * 10)
+			else:
+				input_velocity.x = lerp(velocity.x, 0.0, delta * 10.0)
+				input_velocity.z = lerp(velocity.z, 0.0, delta * 10.0)
+		else:                                                                                       #Otherwise (ie while jumping)
+			input_velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)                   #Incrementally decrease movement with lerp
+			input_velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+	
+	
+	input_velocity.x += external_force.x
+	input_velocity.z += external_force.z
+	
+	velocity.x = input_velocity.x
+	velocity.z = input_velocity.z
+	
+	external_force = external_force.lerp(Vector3.ZERO, delta * 5.0)
+	
+	#Head Bob
+	t_bob += delta * velocity.length() * float(is_on_floor())                                       #Incrementing t_bob as the speed of player with is_on_floor ensuring bobs occur only on ground/while not jumping
+	camera.transform.origin = _headbob(t_bob)                                                       #Moves camera using the value derived from _headbob() function
+	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)                          #Some shi idrk, for changing the fov though
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
+	move_and_slide()
+
+
+#Function for Head Bob
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y = sin(time * BOB_FREQ) * BOB_AMP                                                          #Idek some shit for the up and down head bob
+	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP                                                      #Still don't know, adds a horizontal head bob effect
+	return pos
+
+func hit(dir):
+	emit_signal("player_hit")
+	external_force += dir * HIT_STAGGER
